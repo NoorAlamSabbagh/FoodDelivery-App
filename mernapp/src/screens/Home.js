efde3aff65ff8a27f713088f45c2163bffbbf215
Noor Alam <alamrangrej787@gmail.com>
2023-04-10 14:02:34 +0530

    DisplayData

--- mernapp/src/screens/Home.js
+++ mernapp/src/screens/Home.js
@@ -1,27 +1,27 @@
 import React, { useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import Card from "../components/Card"
+import Card from "../components/Card";
 // import { Card } from "react-bootstrap";
 
 export default function Home() {
   const [foodCat, setFoodCat] = useState([]);
   const [foodItems, setFoodItems] = useState([]);
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
 
   const loadFoodItems = async () => {
     let response = await fetch("http://localhost:5000/api/FoodItems", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-      }
+      },
     });
     response = await response.json();
     //  console.log(response[1][0].CategoryName)
     setFoodItems(response[0]);
     setFoodCat(response[1]);
     // console.log(response[0], response[1])
-  }
+  };
 
   useEffect(() => {
     loadFoodItems();
@@ -33,72 +33,80 @@ export default function Home() {
         <Navbar />
       </div>
       <div>
-      <div
-        id="carouselExampleFade"
-        className="carousel slide carousel-fade"
-        data-bs-ride="carousel">
-        <div className="carousel-inner " id="carousel">
-          <div className=" carousel-caption  " style={{ zIndex: "9" }}>
-            <div className=" d-flex justify-content-center">
-              {/* justify-content-center, copy this <form> from navbar for search box */}
-              <input
-                className="form-control me-2 w-75 bg-white text-dark"
-                type="search"
-                placeholder="Search in here..." 
-                aria-label="Search" value={search} onChange={(e)=>{setSearch(e.target.value)}}
-              />
-              {/* <button className="btn text-white bg-success text-white bg-success" type="submit">
+        <div
+          id="carouselExampleFade"
+          className="carousel slide carousel-fade"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-inner " id="carousel">
+            <div className=" carousel-caption  " style={{ zIndex: "9" }}>
+              <div className=" d-flex justify-content-center">
+                {/* justify-content-center, copy this <form> from navbar for search box */}
+                <input
+                  className="form-control me-2 w-75 bg-white text-dark"
+                  type="search"
+                  placeholder="Search in here..."
+                  aria-label="Search"
+                  value={search}
+                  onChange={(e) => {
+                    setSearch(e.target.value);
+                  }}
+                />
+                {/* <button className="btn text-white bg-success text-white bg-success" type="submit">
                 Search
               </button> */}
+              </div>
+            </div>
+            <div className="carousel-item active">
+              <img
+                src="https://source.unsplash.com/random/900×700/?burger"
+                className="d-block w-100"
+                style={{ filter: "brightness(30%)" }}
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://source.unsplash.com/random/900×700/?pizza"
+                className="d-block w-100"
+                style={{ filter: "brightness(30%)" }}
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://source.unsplash.com/random/900×700/?barbeque"
+                className="d-block w-100"
+                style={{ filter: "brightness(30%)" }}
+                alt="..."
+              />
             </div>
           </div>
-          <div className="carousel-item active">
-            <img
-              src="https://source.unsplash.com/random/900×700/?burger"
-              className="d-block w-100"  style={{ filter: "brightness(30%)" }}
-              alt="..."
-            />
-          </div>
-          <div className="carousel-item">
-            <img
-              src="https://source.unsplash.com/random/900×700/?pizza"
-              className="d-block w-100"  style={{ filter: "brightness(30%)" }}
-              alt="..."
-            />
-          </div>
-          <div className="carousel-item">
-            <img
-              src="https://source.unsplash.com/random/900×700/?barbeque"
-              className="d-block w-100"  style={{ filter: "brightness(30%)" }}
-              alt="..."
-            />
-          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleFade"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleFade"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
         </div>
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleFade"
-          data-bs-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleFade"
-          data-bs-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
       </div>
       <div className="container">
         {foodCat !== []
@@ -110,24 +118,30 @@ export default function Home() {
                   </div>
                   <hr />
                   {foodItems !== [] ? (
-                    foodItems.filter(
-                      ((items) => (items.CategoryName === data.CategoryName)  && (items.name.toLowerCase().includes(search.toLocaleLowerCase())))
-                    ).map((filterItems) => {
-                      return (
-                        <div
-                          key={filterItems._id}
-                          className="col-12 col-md-6 col-lg-3"
-                        >
-                          {console.log(filterItems.url)}
-                          <Card
-                            foodName={filterItems.name}
-                            item={filterItems}
-                            options={filterItems.options[0]}
-                            ImgSrc={filterItems.img}
-                          ></Card>
-                        </div>
-                      );
-                    })
+                    foodItems
+                      .filter(
+                        (items) =>
+                          items.CategoryName === data.CategoryName &&
+                          items.name
+                            .toLowerCase()
+                            .includes(search.toLocaleLowerCase())
+                      )
+                      .map((filterItems) => {
+                        return (
+                          <div
+                            key={filterItems._id}
+                            className="col-12 col-md-6 col-lg-3"
+                          >
+                            {console.log(filterItems.url)}
+                            <Card
+                              foodName={filterItems.name}
+                              item={filterItems}
+                              options={filterItems.options[0]}
+                              ImgSrc={filterItems.img}
+                            ></Card>
+                          </div>
+                        );
+                      })
                   ) : (
                     <div>No such data found </div>
                   )}
