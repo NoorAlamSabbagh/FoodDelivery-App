003e0edd40625bd4bea777011b26bbb4658709c5
Noor Alam <alamrangrej787@gmail.com>
2023-06-04 14:13:34 +0530

    update backend

--- mernapp/backend/db.js
+++ mernapp/backend/db.js
@@ -1,15 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
 const mongoDB = () => {
-mongoose.connect("mongodb://localhost:27017/AlamFood")
-.then(() => {
-    console.log(`database connection successful`);
-    const foodDataPromise = mongoose
-           .connection.db.collection("FoodItems")
+  // console.log(process.env.MONGODB_URL)
+  
+  mongoose
+    .connect(process.env.BACK_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    .then(() => {
+      console.log("Database connected successfully");
+      const foodDataPromise = mongoose
+           .connection.db.collection("foodData")
         .find({})
         .toArray();
         // console.log(foodDataPromise);
         const foodCategoryPromise = mongoose
-                .connection.db.collection("FoodCategory")
+                .connection.db.collection("foodCategory")
                 .find({})
                 .toArray();
                 // console.log(foodCategoryPromise);   
@@ -39,65 +46,3 @@ module.exports = mongoDB;
 
 
 
-
-
-// //
-// const mongoose = require('mongoose');
-
-// const mongoDB = () => {
-//   mongoose
-//   .connect("mongodb://localhost:27017/AlamFood")
-//     .then(() => {
-//       console.log("Database connected successfully")
-    
-//     //   const foodDataPromise = mongoose
-//     //     .connection.db.collection("FoodItems")
-//     //     .find({})
-//     //     .toArray();
-//     //   const foodCategoryPromise = mongoose
-//     //     .connection.db.collection("FoodCategory")
-//     //     .find({})
-//     //     .toArray();
-//     //   Promise.all([foodDataPromise, foodCategoryPromise])
-//     //     .then(([FoodItems, FoodCategory]) => {
-//     //       global.FoodItems = FoodItems;
-//     //       global.FoodCategory = FoodCategory;
-//     //     //   console.log("Food data:", global.FoodItems);
-//     //     //   console.log("Food category:", global.foodCategory);
-//     //     })
-//         // .catch((error) => {
-//         //   console.log("Error while fetching data:", error);
-//         // });
-//     })
-//     .catch((error) => {
-//       console.log("Error while connecting to the database:", error);
-//     });
-// };
-
-// module.exports = mongoDB;
-
-
-
-
-
-//
-// const mongoose = require('mongoose');
-// const {connect} = require('mongoose');
-
-// const mongoDB = async() =>{
-// await mongoose.connect('mongodb://localhost:27017/AlamFood')
-// .then(() => {
-//     console.log(`connection with DB successful`);
-//     const fetched_data =  mongoose.connections.db.collection("FoodItems");
-//     fetched_data.find({}).toArray(function(err, data){
-//         if(err) console.log(err);
-//         else{
-//             global.food_items = data;
-//             console.log(global.food_items);
-//         }
-//     })
-// })
-// .catch(() => {
-//     console.log(`something went wrong!!`);
-// })
-// }
